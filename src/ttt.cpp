8c3e9f4f5b0d6b8cdb1db2139a28bfaecc11ea5f
wobak <wobakjerna@web.de>
2015-05-17 22:06:30 +0200

    iterate collected devices and find devices usable for 4 channel recording

--- src/ttt.cpp
+++ src/ttt.cpp
@@ -97,9 +97,10 @@ int main(int argc, char *argv[])
     char* name = snd_device_name_get_hint(*n, "NAME");
     char* io = snd_device_name_get_hint(*n, "IOID");
 
-    if(name != NULL && strcmp("null", name) != 0) {
+    if(name != NULL && strcmp("null", name) != 0 && strcmp("null", name)) {
       if(io == NULL || strcmp("Output", io) != 0) {
         devices.push_back(name);
+        if(io != NULL)std::cout << name << " " << std::string{io} << std::endl;
       }
       free(name);
       free(io);
@@ -111,52 +112,65 @@ int main(int argc, char *argv[])
     std::cout << std::string{device} << std::endl;
   }
 
+  std::cout << "----testing PCMs----------------------------------------------" << std::endl;
+  unsigned num_channels = 4;
   snd_pcm_t *capture_handle;
-  err = snd_pcm_open(&capture_handle, devices[0].c_str(), SND_PCM_STREAM_CAPTURE, 0);
-  if(err < 0) {
-    std::cerr << "cannot open audio device " << argv[1] << " " << snd_strerror(err) << std::endl;
-    return 1;
-  }
-     
   snd_pcm_hw_params_t *hw_params;
   err = snd_pcm_hw_params_malloc(&hw_params);
   if(err < 0) {
     std::cerr << "cannot allocate hardware parameter structure " << snd_strerror(err) << std::endl;
     return 1;
   }
-       
-  err = snd_pcm_hw_params_any(capture_handle, hw_params);
-  if(err < 0) {
-    std::cerr << "cannot initialize hardware parameter structure " << snd_strerror(err) << std::endl;
-    return 1;
+
+  std::vector<std::string> correct_devices{};
+  for(auto const& device : devices) {
+    err = snd_pcm_open(&capture_handle, device.c_str(), SND_PCM_STREAM_CAPTURE, 0);
+    if(err < 0) {
+      std::cerr << "cannot open audio device " << device<< " " << snd_strerror(err) << std::endl;
+      continue;
+    }
+    else {
+      std::cout << "Starting configuration on " << device << std::endl;
+    }
+         
+    err = snd_pcm_hw_params_any(capture_handle, hw_params);
+    if(err < 0) {
+      std::cerr << "cannot initialize hardware parameter structure " << snd_strerror(err) << std::endl;
+      continue;
+    }
+
+    err = snd_pcm_hw_params_set_access(capture_handle, hw_params, SND_PCM_ACCESS_RW_INTERLEAVED);
+    if(err < 0) {
+      std::cerr << "cannot set access type " << snd_strerror(err) << std::endl;
+      continue;
+    }
+
+    err = snd_pcm_hw_params_set_format(capture_handle, hw_params, SND_PCM_FORMAT_S32_LE);
+    if(err < 0) {
+      std::cerr << "cannot set sample format " << snd_strerror(err) << std::endl;
+      continue;
+    }
+
+    unsigned rate = 48000;
+    err = snd_pcm_hw_params_set_rate_near(capture_handle, hw_params, &rate, 0);
+    if(err < 0) {
+      std::cerr << "cannot set sample rate " << snd_strerror(err) << std::endl;
+      continue;
+    }
+
+    err = snd_pcm_hw_params_set_channels(capture_handle, hw_params, num_channels);
+    if(err < 0) {
+      std::cerr << "cannot set channel count " << snd_strerror(err) << std::endl;
+      continue;
+    }
+
+    correct_devices.push_back(device);
   }
 
-  err = snd_pcm_hw_params_set_access(capture_handle, hw_params, SND_PCM_ACCESS_RW_INTERLEAVED);
-  if(err < 0) {
-    std::cerr << "cannot set access type " << snd_strerror(err) << std::endl;
-    return 1;
+  std::cout << "----suitable PCMs----------------------------------------------" << std::endl;
+  for(auto device : correct_devices) {
+    std::cout << device << std::endl;
   }
-
-  err = snd_pcm_hw_params_set_format(capture_handle, hw_params, SND_PCM_FORMAT_S32_LE);
-  if(err < 0) {
-    std::cerr << "cannot set sample format " << snd_strerror(err) << std::endl;
-    return 1;
-  }
-
-  unsigned rate = 48000;
-  err = snd_pcm_hw_params_set_rate_near(capture_handle, hw_params, &rate, 0);
-  if(err < 0) {
-    std::cerr << "cannot set sample rate " << snd_strerror(err) << std::endl;
-    return 1;
-  }
-
-  unsigned num_channels = 4;
-  err = snd_pcm_hw_params_set_channels(capture_handle, hw_params, num_channels);
-  if(err < 0) {
-    std::cerr << "cannot set channel count " << snd_strerror(err) << std::endl;
-    return 1;
-  }
-
   // install configuration, calls "snd_pcm_prepare" on handle automatically
   err = snd_pcm_hw_params(capture_handle, hw_params);
   if(err < 0) {
@@ -179,5 +193,7 @@ int main(int argc, char *argv[])
 
   snd_pcm_close(capture_handle);
   std::cout << "success" << std::endl;
+
+
   return 0;
 }
