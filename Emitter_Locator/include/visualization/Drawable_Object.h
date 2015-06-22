e9f326771aa5aca4c90d7dfb595fdf5a3e9c32c4
wobakj <wobakjerna@web.de>
2015-06-22 12:16:22 +0200

    better circle movement & circle-table intersection prototype

--- Emitter_Locator/include/visualization/Drawable_Object.h
+++ Emitter_Locator/include/visualization/Drawable_Object.h
@@ -14,13 +14,13 @@ class Drawable_Object {
 
 		virtual void Draw(sf::RenderWindow& canvas) const = 0;
 		//virtual void Recalculate_Geometry(sf::Vector2f const& resolution) = 0;
-
+		static sf::Vector2f const& get_phys_table_size() { return physical_table_size_;};
 	protected:
 
 		//scaling factor for the elements attributes
 		static float pixel_per_meter_;
-		static sf::Vector2f table_dims_in_px_;
 		static sf::Vector2f physical_table_size_;
+		static sf::Vector2f table_dims_in_px_;
 		static sf::Vector2u resolution_;
 
 };
