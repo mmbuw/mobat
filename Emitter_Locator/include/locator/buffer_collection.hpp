a86d1750baf817d8ecc1315d9d1389e897688d99
wobakj <jakob.wagner@uni-weimar.de>
2015-06-29 22:37:00 +0200

    correct buffer_collection length member and add extra member for length in bytes

--- Emitter_Locator/include/locator/buffer_collection.hpp
+++ Emitter_Locator/include/locator/buffer_collection.hpp
@@ -1,53 +1,31 @@
 #ifndef BUFFERCOLLECTION_HPP
 #define BUFFERCOLLECTION_HPP
 
-struct buffer_collection {
-    buffer_collection(unsigned long bufferlength, std::size_t buffernum) :
-     count{buffernum},
-     length{bufferlength}
-    {
-        buffers = (int**)malloc(buffernum * sizeof(int*));
+#include <cstdlib>
+#include <vector>
 
-        for(std::size_t i = 0; i < count; ++i) {
-            buffers[i] = (int*)malloc(bufferlength);
-        }
-    }
+struct buffer_collection {
+    buffer_collection(std::size_t bufferbytes, std::size_t buffernum);
 
     //block implicit copy operations 
     buffer_collection(buffer_collection const&) = delete;
     buffer_collection& operator=(buffer_collection const&) = delete;
 
-    ~buffer_collection() {
-        for(std::size_t i = 0; i < count; ++i) {
-            free(buffers[i]);
-            buffers[i] = nullptr;
-        }
-        free(buffers);
-    }
-
+    ~buffer_collection();
     //copy data from interleaved buffer, assumes that the sourcesize equals length * count 
-    void from_interleaved(unsigned char* source) {
+    void from_interleaved(unsigned char* source);
 
-        for(std::size_t current_buffer = 0; current_buffer < count; ++current_buffer) {
-
-           for(std::size_t buffer_offset_pos = 0; buffer_offset_pos < length /4; ++buffer_offset_pos) {
-
-                buffers[current_buffer][buffer_offset_pos] 
-                    = 0x0 
-                    | source[buffer_offset_pos * (count*4)      + current_buffer * 4 ]  << 0
-                    | source[buffer_offset_pos * (count*4) + 1  + current_buffer * 4 ]  << 8
-                    | source[buffer_offset_pos * (count*4) + 2  + current_buffer * 4 ]  << 16
-                    | source[buffer_offset_pos * (count*4) + 3  + current_buffer * 4 ]  << 24;
-            }
-        }
-    }
-
-    int* const& operator[](std::size_t i) {
-        return buffers[i];
-    }
+    int* const& operator[](std::size_t i);
+    int* const& operator[](std::size_t i) const;
+    // std::vector<int>& operator[](std::size_t i);
+    // std::vector<int> const& operator[](std::size_t i) const;
 
     std::size_t count;
-    unsigned long length;
+    // length in bytes
+    std::size_t bytes;
+    // length in ints
+    std::size_t length;
     int** buffers;
+    std::vector<std::vector<int>> buffers2;
 };
 #endif //BUFFERCOLLECTION_HPP
---
+++ Emitter_Locator/src/locator/buffer_collection.cpp
@@ -0,0 +1,57 @@
+#include "buffer_collection.hpp"
+#include <stdexcept>
+
+buffer_collection::buffer_collection(std::size_t bufferbytes, std::size_t buffernum) :
+ count{buffernum},
+ bytes{bufferbytes},
+ length{bufferbytes / 4}
+{
+	buffers = (int**)malloc(buffernum * sizeof(int*));
+
+	if(buffers) {
+		for(std::size_t i = 0; i < count; ++i) {
+			buffers[i] = (int*)malloc(bufferbytes);
+			if(!buffers[i]) {
+				throw std::logic_error("memory allocation failed");
+			}
+		}
+	}
+	else throw std::logic_error("memory allocation failed");
+}
+
+buffer_collection::~buffer_collection() {
+	for(std::size_t i = 0; i < count; ++i) {
+		if(buffers[i]) {
+			free(buffers[i]);
+			buffers[i] = nullptr;
+		}
+	}
+	if(buffers) {
+		free(buffers);
+	}
+}
+
+//copy data from interleaved buffer, assumes that the sourcesize equals length * count 
+void buffer_collection::from_interleaved(unsigned char* source) {
+
+	for(std::size_t current_buffer = 0; current_buffer < count; ++current_buffer) {
+
+	   for(std::size_t buffer_offset_pos = 0; buffer_offset_pos < length /4; ++buffer_offset_pos) {
+
+			buffers[current_buffer][buffer_offset_pos] 
+				= 0x0 
+				| source[buffer_offset_pos * (count*4)      + current_buffer * 4 ]  << 0
+				| source[buffer_offset_pos * (count*4) + 1  + current_buffer * 4 ]  << 8
+				| source[buffer_offset_pos * (count*4) + 2  + current_buffer * 4 ]  << 16
+				| source[buffer_offset_pos * (count*4) + 3  + current_buffer * 4 ]  << 24;
+		}
+	}
+}
+
+int* const& buffer_collection::operator[](std::size_t i) {
+	return buffers[i];
+}
+
+int* const& buffer_collection::operator[](std::size_t i) const {
+	return buffers[i];
+}
