f7c93efd7de8f6c2bb4c1ff5c1ca6d8a47c7b85b
Nyran <adrian.kreskowski@uni-weimar.de>
2015-10-01 00:00:01 +0200

    add MobatServer class and app

--- mobat/networking/token_position.hpp
+++ mobat/networking/token_position.hpp
@@ -6,19 +6,22 @@
 struct token_position {
   token_position()
    :id{0}
-   ,x{0}
-   ,y{0}
+   ,x{0.0f}
+   ,y{0.0f}
+   ,timestamp{0}
   {}
 
-  token_position(std::uint8_t i, float xx, float yy)
+  token_position(std::uint32_t i, float xx, float yy, std::uint32_t ts)
    :id{i}
    ,x{xx}
    ,y{yy}
+   ,timestamp{ts}
   {}
 
-  std::uint8_t id;
+  std::uint32_t id;
   float x;
   float y;
+  std::uint32_t timestamp;
 };
 
 #endif
