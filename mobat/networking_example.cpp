a63fc7b63a5a3aa462f0206d53ad77102404bdf7
Nyran <adrian.kreskowski@uni-weimar.de>
2015-10-01 10:59:04 +0200

    rename 'token_position' to 'token_packet' and add ostream operator

--- mobat/networking_example.cpp
+++ mobat/networking_example.cpp
@@ -4,7 +4,7 @@
 #include <cstring>
 #include "address.hpp"
 #include "packet.hpp"
-#include "token_position.hpp"
+#include "token_packet.hpp"
 
 int main(int argc, char *argv[]) {
 
@@ -13,8 +13,8 @@ int main(int argc, char *argv[]) {
     Socket socket{6666};
     Address_ip4 target_address{127, 0, 0, 1, 30000};
 
-    token_position test{1, 1337.0f, 1138.0f};
-    packet::send<token_position>(socket, target_address, test);
+    token_packet test{1, 1337.0f, 1138.0f};
+    packet::send<token_packet>(socket, target_address, test);
   }
   else {
     std::cout << "receiver" << std::endl;
@@ -23,8 +23,8 @@ int main(int argc, char *argv[]) {
     while (true){
       Address_ip4 source_address{};
       
-      token_position received_pos{};
-      bool got_package = packet::receive<token_position>(socket, &source_address, &received_pos);
+      token_packet received_pos{};
+      bool got_package = packet::receive<token_packet>(socket, &source_address, &received_pos);
       if (got_package) {
         std::cout << "received from ip " << source_address.address() << " from port " << source_address.port() << std::endl;
         std::cout << "id " << received_pos.id << ", pos (" << received_pos.x << ", " << received_pos.y << ")" << std::endl;
