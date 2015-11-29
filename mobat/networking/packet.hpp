8f801910408dc31b5a2c12d8380582d2a24b424b
JBPunkt <jannis-bossert@web.de>
2015-11-29 17:49:42 +0100

    removed footer and header from packet in order to support communication with sfml sockets

--- mobat/networking/packet.hpp
+++ mobat/networking/packet.hpp
@@ -18,20 +18,8 @@ struct packet_t {
    :value(val)
   {}
 
-  const std::uint32_t header = correct_header;
   T value;
-  const std::uint8_t footer = correct_footer; 
-  
-  // static values to check for correctness of packet
-  static const std::uint32_t correct_header;
-  static const std::uint8_t correct_footer;
 };
-
-// make header depending on hash of contained struct
-template<typename T>
-const std::uint32_t packet_t<T>::correct_header = std::uint32_t(typeid(T).hash_code());
-template<typename T>
-const std::uint8_t packet_t<T>::correct_footer = 116;
 
 // send a packet containing object to type T from socket to address
 template<typename T>
@@ -48,15 +36,6 @@ bool receive(Socket const& socket, Address* received_address, T* received_val) {
   ssize_t received_bytes = socket.receive(received_address, (uint8_t*)&received_packet, sizeof(received_packet));
 
   if (received_bytes != sizeof(received_packet)) {
-    return false;
-  }
-
-  if (received_packet.header != packet_t<T>::correct_header) {
-    return false;
-  }
-
-  if (received_packet.footer != packet_t<T>::correct_footer) {
-    throw std::runtime_error("Packet corrupted");
     return false;
   }
 
